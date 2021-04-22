86f8d14c9d1c11a518f6d5ac001cc9d6723ac86f
Dovilė Bilkštienė <dote@MacBook-Air.lan>
2021-04-22 15:56:58 +0300

    truputi daugiau visko

--- script.js
+++ script.js
@@ -4,10 +4,44 @@ document.getElementById('mobile-menu-trigger').addEventListener('click', functio
     document.getElementById('navigation').classList.toggle('active');
 });
 
+// tab navigation 
+
+let tabNavigationBlocks = document.querySelectorAll('.tab-section nav ul li');
+let tabContentBlocks = document.querySelectorAll('.tab-section .tab-content')
+
+function removeActiveNavigation() {
+    for (let tab of tabNavigationBlocks) {
+        tab.classList.remove('active');
+    }
+}
+
+function removeActiveTabs() {
+    for (let tab of tabContentBlocks) {
+        tab.classList.remove('active');
+    }
+}
+
+for (let tab of tabNavigationBlocks) {
+    tab.addEventListener('click', function () {
+
+        removeActiveNavigation();
+        removeActiveTabs();
+
+        this.classList.add('active');
+
+        let index = this.dataset.slide;
+
+        document.querySelector('.tab-section .tab-content[data-slide="' + index + '"]').classList.add('active');
+
+        // swiper.slideTo(index - 1);
+        // su situo reguliuojasi apatinis swiperis. reiktu nauja pasikurt
+    })
+}
+
 // reviews swiper
 
 const swiper = new Swiper('.reviews-swiper', {
-    // by default .swiper-container, bet dedu saco klase, kad galeciau pernaudoti
+    // by default .swiper-container, bet dedu savo klase, kad galeciau pernaudoti
     slidesPerView: 1,
     // slidesPerGroup: 3,
     // spaceBetween: 120,
